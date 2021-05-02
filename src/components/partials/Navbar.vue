f3046db20838271ad88aaa0c77ee7684f576e122
cindrmon1899 <feroxafarensis019@protonmail.com>
2021-05-02 08:42:20 +0800

    edit file layout

---
+++ src/components/partials/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="nav">
+    <router-link :to="{ name: 'Home' }">Home</router-link> |
+    <router-link :to="{ name: 'About' }">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style></style>
