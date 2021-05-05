6fa083de73f45ddaa12e10dbe2f2d2061fe0f521
cindrmon1899 <feroxafarensis019@protonmail.com>
2021-05-05 21:12:04 +0800

    use supabase for database of content and add to website

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueSupabase from "vue-supabase";
 import "@/assets/styles/main.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueSupabase, {
+  supabaseUrl: "https://pquqeicmdkarhgevtbeu.supabase.co",
+  supabaseKey: process.env.VUE_APP_SUPABASE_KEY,
+  supabaseOptions: {},
+});
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="home" class="flex h-screen">
-    <div class="m-auto align-middle text-center py-52">
+    <div
+      v-if="selectedCatName !== undefined && selectedCatTrait !== undefined"
+      class="m-auto align-middle text-center py-52"
+    >
       <span>My name is</span><br />
-      <span class="text-cyan">{{ catName }}</span
+      <span class="text-cyan">{{ selectedCatName }}</span
       ><br />
       <span>and I am the</span><br />
-      <span class="text-cyan">{{ catTrait }}</span
+      <span class="text-cyan">{{ selectedCatTrait }}</span
       ><br />
       <span>cat</span><br />
     </div>
@@ -17,6 +20,33 @@
 
 export default {
   name: "Home",
+  data() {
+    return {
+      catNames: [],
+      catTraits: [],
+    };
+  },
+  computed: {
+    selectedCatName() {
+      let randomValue = Math.floor(Math.random() * this.catNames.length);
+      return this.catNames[randomValue];
+    },
+    selectedCatTrait() {
+      let randomValue = Math.floor(Math.random() * this.catTraits.length);
+      return this.catTraits[randomValue];
+    },
+  },
+  async created() {
+    const { data: catNames } = await this.$supabase
+      .from("cat_names")
+      .select("name");
+    const { data: catTraits } = await this.$supabase
+      .from("cat_traits")
+      .select("name");
+
+    this.catNames = catNames.map((item) => item.name);
+    this.catTraits = catTraits.map((item) => item.name);
+  },
 };
 </script>
 
