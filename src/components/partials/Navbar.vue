d5e29faef6368c415d5fbd17bd8d41fcac522f9c
cindrmon1899 <feroxafarensis019@protonmail.com>
2021-05-05 20:09:11 +0800

    fix scaling of text for navbar and main text

--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="nav" class="container mx-auto text-center p-5 text-serif">
     <router-link
-      class="text-white no-underline text-sans-serif"
+      class="text-white no-underline text-xs sm:text-base"
       :to="{ name: 'Home' }"
       >Generate</router-link
     >
     |
-    <router-link class="text-white no-underline" :to="{ name: 'Submit' }"
+    <router-link
+      class="text-white text-xs sm:text-base no-underline"
+      :to="{ name: 'Submit' }"
       >Submit Your Own</router-link
     >
     |
-    <router-link class="text-white no-underline" :to="{ name: 'About' }"
+    <router-link
+      class="text-white text-xs sm:text-base no-underline"
+      :to="{ name: 'About' }"
       >About</router-link
     >
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,5 +25,8 @@ export default {
   font-family: cooper-black-std, serif;
   color: white;
   font-size: 5rem;
+  @media screen and (max-width: 768px) {
+    font-size: 3.25rem;
+  }
 }
 </style>
